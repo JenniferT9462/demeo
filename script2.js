292c1a44a54b36b60e99ccd641b599d9f538752a
Jennifer Tarleton <jennifertarleton@gmail.com>
2026-06-12 17:40:08 -0500

    fixes

--- script2.js
+++ script2.js
@@ -1,52 +1,8 @@
-
-
-
-// const url = `https://www.stands4.com/services/v2/quotes.php?uid=${account_id}&tokenid=${token}&format=json`
-
-
-
-// const formTag = document.getElementById("formTag");
-// formTag.onsubmit = fetchQuotes;
-
-// const output = document.getElementById("output")
-
-
-
-// async function fetchQuotes(event) {
-//     event.preventDefault();
-
-//     const form = event.target;
-//     const searchType = form.elements.searchType.value;
-//     const query = form.elements.searchQuery.value;
-
-//     const data = {
-//       searchtype: searchType,
-//       query: query,
-//     };
-
-//     const dataString = new URLSearchParams(data)
-//     console.log(dataString);
-
-//     const response = await fetch(url + "&" + dataString);
-//     console.log(response);
-//     const result = await response.json();
-//     console.log(result);
-//     output.innerHTML = `
-//         <div>
-//             <h2>Quote: ${result.result[0].quote}</h2>
-//             <p>Author: ${result.result[0].author}</p>
-//         </div>
-//     `
-    
-
-
-// }
-
 const account_id = 13732;
-const token = 'Zs3vQtt1k7oBnQQp';
+const token = "Zs3vQtt1k7oBnQQp";
 
-const base_url = `https://www.stands4.com/services/v2/quotes.php?uid=${account_id}&tokenid=${token}&format=json`
-//&searchtype=AUTHOR&query=Albert+Einstein
+const base_url = `https://www.stands4.com/services/v2/quotes.php?uid=${account_id}&tokenid=${token}&format=json`;
+
 
 const formTag = document.getElementById("formTag");
 formTag.onsubmit = fetchQuotes;
@@ -54,35 +10,70 @@ formTag.onsubmit = fetchQuotes;
 const output = document.getElementById("output");
 
 async function fetchQuotes(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const form = event.target;
-    const searchType = form.elements.searchType.value;
-    const searchQuery = form.elements.searchQuery.value;
+  const form = event.target;
+  const searchType = form.elements.searchType.value;
+  const searchQuery = form.elements.searchQuery.value;
 
-    const data = {
-        searchtype: searchType,
-        query: searchQuery
-    }
+  const data = {
+    searchtype: searchType,
+    query: searchQuery,
+  };
 
-    const dataString = new URLSearchParams(data);
-    console.log(dataString);
+  // URLSearchParams output: searchtype=AUTHOR&query=Albert+Einstein
+  const dataString = new URLSearchParams(data);
+  console.log(dataString);
 
-    const response = await fetch(base_url + "&" + dataString);
-    console.log(response);
+  const response = await fetch(base_url + "&" + dataString);
+  console.log(response);
 
-    const result = await response.json();
-    console.log(result);
+  const result = await response.json();
+  console.log(result);
 
-    output.innerHTML = `
-            <div>
-                <h2>Quote: ${result.result[0].quote}</h2>
-                <p>Author: ${result.result[0].author}</p>
-            
-            </div>
-    
-    `;
+  let quoteData;
+  if (Array.isArray(result.result)){
+    quoteData = result.result[0];
+  } else {
+    quoteData = result.result;
+  }
+  
 
-
-
+  output.innerHTML = `
+        <div>
+           <h2>Quote: ${quoteData.quote}</h2>
+           <p>Author: ${quoteData.author}</p>
+        </div>
+  `;
 }
+
+
+
+
+
+
+//This checks whether the API returned an array (author/keyword search)
+// or a plain object (random quote), and pulls the data correctly either way.
+// const quote = Array.isArray(result.result) ? result.result[0] : result.result;
+
+//    const dataReceived = await response.json();
+//     console.log(dataReceived);
+
+//     const quotes = dataReceived.result;
+//     console.log(quotes);
+
+//     let limitedQuotes = [];
+//     if(quotes.length > 5){
+//         limitedQuotes = quotes.slice(0, 5);
+
+//     }
+
+//    for(let i = 0; i < limitedQuotes.length; i++) {
+//         output.innerHTML += `
+//             <div>
+//                 <h2>Quote: ${limitedQuotes[i].quote}</h2>
+//                 <p>Author: ${limitedQuotes[i].author}</p>
+
+//             </div>
+
+//     `;
